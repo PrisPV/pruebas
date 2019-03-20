ae59de2acac38247de576ed88fb6f3af491af5b6
PrisPV <erikaprispi@gmail.com>
2019-03-20 18:04:20 -0500

    view-report

--- resources/views/areas/show.blade.php
+++ resources/views/areas/show.blade.php
@@ -5,9 +5,26 @@
 @section('content')
 <div class="card">
 <p>hola</p>
-<div class="card-header">Area: {{$area->name}}</div>
+<div class="card-header">Reporte</div>
   <div class="card-body">
-    <p><strong>Nombre</strong> {{$area->nombre}}</p>
+    <table id="areas" class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th>Area</th>
+          <th>Asignatura</th>
+          <th>Cuestionario</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{$area->name}}</td>
+          <td>{{$area->id}}</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+
+
   </div>
 </div>
 @endsection
